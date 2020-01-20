1d703eb5a5a65b452099a9f45d47e2c5920277dc
Imam Satya Wedhatama <14.8182@stis.ac.id>
2020-01-20 20:59:17 +0800

    handphone dan data yang dicari

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -6,7 +6,7 @@
         style="width:30vw;max-width:150px;"
       >
     </p>
-    <p class="text-faded">Hmm X, salah fokus sepertinya...<strong>(404)</strong></p>
+    <p class="text-faded">Hmm, salah fokus sepertinya...<strong>(404)</strong></p>
     <q-btn
       color="secondary"
       style="width:200px;"
